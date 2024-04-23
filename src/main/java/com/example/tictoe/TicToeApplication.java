ef156909c2e7e124e2f531aa86bdc7a49f337d09
Aditya Singh <adi7753071602@gmail.com>
2024-04-23 22:07:08 +0530

    models added

---
+++ src/main/java/com/example/tictoe/TicToeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tictoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicToeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicToeApplication.class, args);
+    }
+
+}
